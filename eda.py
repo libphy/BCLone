eb551d73cc2a281776d257309dfbde969abf7695
libphy <libphy@gmail.com>
2017-01-24 14:29:06 -0700

    testing generator

--- eda.py
+++ eda.py
@@ -7,15 +7,18 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Flatten, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+import os
 
-df = pd.read_csv('driving_log.csv', header=None, names=['center','right','left','angle','accl','break','speed'])
-cpaths=zip(df['center'],df['angle'])
-def gen(paths):
+cwd = os.getcwd()
+df = pd.read_csv(cwd+'/data/driving_log.csv', header=0)#header=None, names=['center','right','left','steering','throttle','brake','speed'])
+# the header setup depends on the data file. check if the data already includes header.
+
+cpaths=zip(df['center'],df['steering'])
+def gen(paths,cwd):
     while 1:
         for fp, ang in paths:
-            # create numpy arrays of input data
-            # and labels, from each line in the file
-            im = mpimg.imread(fp)
+            im = mpimg.imread(cwd+'/data/'+fp)
+            print(fp, im.shape)
             im = np.expand_dims(im,axis=0)
             yield((im, ang))
 
@@ -32,24 +35,24 @@ model.add(Activation('softmax'))
 
 model.compile('adam', 'mse')
 
-model.fit_generator(gen(cpaths),
+model.fit_generator(gen(cpaths,cwd),
         samples_per_epoch=10, nb_epoch=9)
 
 ##
 #http://stackoverflow.com/questions/38936016/keras-how-are-batches-and-epochs-used-in-fit-generator
 
-X = np.empty((0,160,320,3))
-y = []
-i=0
-for fp, ang in cpaths:
-    # create numpy arrays of input data
-    # and labels, from each line in the file
-    im = mpimg.imread(fp)
-    im = np.expand_dims(im,axis=0)
-    X = np.concatenate([X,im],axis=0)
-    y.append(ang)
-    i+=1
-    if i%100==0:
-        print(i)
+# X = np.empty((0,160,320,3))
+# y = []
+# i=0
+# for fp, ang in cpaths:
+#     # create numpy arrays of input data
+#     # and labels, from each line in the file
+#     im = mpimg.imread(fp)
+#     im = np.expand_dims(im,axis=0)
+#     X = np.concatenate([X,im],axis=0)
+#     y.append(ang)
+#     i+=1
+#     if i%100==0:
+#         print(i)
 
 # https://carnd-forums.udacity.com/questions/36051083/p3-some-reflection
