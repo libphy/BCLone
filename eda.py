f0fe3571a6e50c8225d547124c0238a5210835f9
libphy <libphy@gmail.com>
2017-02-20 22:32:40 -0700

    flip

--- eda.py
+++ eda.py
@@ -4,27 +4,28 @@ import matplotlib.image as mpimg
 from keras.models import Model
 import numpy as np
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.core import Dense, Activation, Flatten, Dropout, Lambda
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+from keras.layers.advanced_activations import ELU
 from keras.optimizers import Adam
 import os
 import cv2
 import json
 #from imblearn.over_sampling import SMOTE
 from collections import Counter
-
+from sklearn.utils import shuffle
 # use  CUDA_VISIBLE_DEVICES="0" ipython in the bash to use actual device 1
 
 BATCH_SIZE = 64
 cwd = os.getcwd()
-#df = pd.read_csv(cwd+'/track1/driving_log.csv', header=None, names=['center','right','left','steering','throttle','brake','speed']) #use for user collected data
-df = pd.read_csv(cwd+'/data/driving_log.csv', header=0) #use for udacity data
+df = pd.read_csv(cwd+'/midlane/driving_log.csv', header=None, names=['center','right','left','steering','throttle','brake','speed']) #use for user collected data
+#df = pd.read_csv(cwd+'/data/driving_log.csv', header=0) #use for udacity data
 # the header setup depends on the data file. check if the data already includes header.
 ## change df file addresses to full addresses
 
-impaths = list(map(lambda x: cwd+'/data/'+x, df['center'])) #use for udacity data
-#impaths = list(map(lambda x: '/'.join(x.split('/')[:-2])+'/track1/'+'/'.join(x.split('/')[-2:]), df['center'])) #use for user collected data
+#impaths = list(map(lambda x: cwd+'/data/'+x, df['center'])) #use for udacity data
+impaths = list(map(lambda x: '/'.join(x.split('/')[:-2])+'/midlane/'+'/'.join(x.split('/')[-2:]), df['center'])) #use for user collected data
 dfs=pd.DataFrame()
 dfs['path']=impaths
 dfs['angle']=df['steering']
@@ -90,9 +91,16 @@ def resample(vec,prob, n):
     res = np.random.choice(vec, n, p=prob)
     return res
 
-dfs['prob'] = inversefreq(df['steering'])
-idxselect = resample(range(len(df)), dfs['prob'], 8000)
-dfn = dfs.ix[idxselect]
+## Resampling with inverse frequency
+# dfs['prob'] = inversefreq(df['steering'])
+# idxselect = resample(range(len(df)), dfs['prob'], 8000)
+# dfn = dfs.ix[idxselect]
+
+
+dfn = dfs #dfs[dfs['angle']!=0.0]
+X = dfn['path']
+y = dfn['angle']
+Xtr, ytr = shuffle(X,y,random_state=0)
 
 def eqhGray(X): # equalize histogram gray
     if X.shape[-1] ==3:
@@ -105,7 +113,7 @@ def eqhGray(X): # equalize histogram gray
     Xe = Xe.reshape(X.shape[0],X.shape[1],X.shape[2],1)
     return (Xe-Xe.mean())/255
 
-def gen(paths, angles, batchsz):
+def gen(paths, angles, batchsz): #with flip enabled, it will produce twice many batch size
     assert len(paths)==len(angles), "Number of features and targets don't match."
     while 1:
         for offset in range(0, len(paths), batchsz):
@@ -113,32 +121,57 @@ def gen(paths, angles, batchsz):
             batch_X = np.array(list(map(lambda x: cv2.imread(x), paths[offset:offset+batchsz])))
             # batch_X = eqhGray(np.array(list(map(lambda x: cv2.imread(x), paths[offset:offset+batchsz]))))
             batch_y = np.array(list(map(lambda x: x, angles[offset:offset+batchsz])))
-            yield(batch_X, batch_y)
+            batch_X_flip = np.array(list(map(lambda x: np.fliplr(x),batch_X)))
+            batch_y_flip = -1*batch_y
+            X_bat = np.concatenate((batch_X, batch_X_flip))
+            y_bat = np.concatenate((batch_y, batch_y_flip))
+            yield(X_bat, y_bat)
 
 
+# # My nano VGG model
+# model = Sequential()
+# model.add(Convolution2D(64, 3, 3, input_shape=(160, 320, 3),border_mode='same',init='glorot_normal', activation='relu'))
+# model.add(Convolution2D(64, 3, 3, border_mode='same',init='glorot_normal', activation='relu'))
+# model.add(MaxPooling2D((2, 2),dim_ordering='tf'))
+# #model.add(Dropout(0.5))
+# #model.add(Activation('relu'))
+# model.add(Convolution2D(64, 3, 3, border_mode='same',init='glorot_normal',activation='relu'))
+# model.add(Convolution2D(64, 3, 3, border_mode='same',init='glorot_normal',activation='relu'))
+# model.add(MaxPooling2D((2, 2),dim_ordering='tf'))
+# #model.add(Dropout(0.5))
+# #model.add(Activation('relu'))
+# model.add(Flatten())
+# model.add(Dense(64))
+# model.add(Activation('relu'))
+# model.add(Dense(1))
+# #model.add(Activation('tanh'))
+#
+# adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+# model.compile(loss='mse', optimizer=adam)
+#
+# model.fit_generator(gen(Xtr,ytr, BATCH_SIZE),samples_per_epoch=len(ytr), nb_epoch=10)
 
+### Comma_AI
 model = Sequential()
-model.add(Convolution2D(32, 3, 3, input_shape=(160, 320, 3),border_mode='same',init='glorot_normal', activation='relu'))
-model.add(Convolution2D(32, 3, 3, border_mode='same',init='glorot_normal', activation='relu'))
-model.add(MaxPooling2D((2, 2),dim_ordering='tf'))
-model.add(Dropout(0.5))
-#model.add(Activation('relu'))
-model.add(Convolution2D(64, 3, 3, border_mode='same',init='glorot_normal',activation='relu'))
-model.add(Convolution2D(64, 3, 3, border_mode='same',init='glorot_normal',activation='relu'))
-model.add(MaxPooling2D((2, 2),dim_ordering='tf'))
-model.add(Dropout(0.5))
-#model.add(Activation('relu'))
+model.add(Lambda(lambda x: x/127.5 - 1.,
+        input_shape=(160, 320, 3),
+        output_shape=(160, 320, 3)))
+model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
+model.add(ELU())
+model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
+model.add(ELU())
+model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode="same"))
 model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
+model.add(Dropout(.2))
+model.add(ELU())
+model.add(Dense(512))
+model.add(Dropout(.5))
+model.add(ELU())
 model.add(Dense(1))
-model.add(Activation('tanh'))
 
-adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-model.compile(loss='mse', optimizer=adam)
+model.compile(optimizer="adam", loss="mse")
 
-model.fit_generator(gen(dfn['path'],dfn['angle'], BATCH_SIZE),samples_per_epoch=len(dfn['angle']), nb_epoch=10)
-
+model.fit_generator(gen(Xtr,ytr, BATCH_SIZE),samples_per_epoch=len(ytr), nb_epoch=10)
 ##
 #http://stackoverflow.com/questions/38936016/keras-how-are-batches-and-epochs-used-in-fit-generator
 
@@ -150,11 +183,11 @@ model.fit_generator(gen(dfn['path'],dfn['angle'], BATCH_SIZE),samples_per_epoch=
 # intermediate_output = intermediate_layer_model.predict(data)
 
 ### EDA with dummy model
-g = gen(impaths[1000:2000], angles[1000:2000], 128) #take some data in the middle to avoid a lot of zero angles in the beginning
+g = gen(impaths[2000:3000], angles[2000:3000], 128) #take some data in the middle to avoid a lot of zero angles in the beginning
 g0 = next(g) #generate data as tuple
-X, y = g0
+X1, y1 = g0
 #y = y.reshape((128,2))
-yp = model.predict(X, batch_size=128) #comparing yp and y by eyes suggest that the model+training sucks.
+yp = model.predict(X1, batch_size=128) #comparing yp and y by eyes suggest that the model+training sucks.
 ## Comment: it could be combination of these
 ## 1) model is too primitive,
 ## 2) needs data augmentation and preprocessing,
